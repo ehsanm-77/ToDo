665893eae697f3074fcd19f6d2a7efe1abcb2cf8
ehsanm <ehsanmohamadi780@gmail.com>
2023-09-29 17:28:55 +0330

    add font and dark mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,36 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import '@/styles/globals.css';
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline, PaletteMode, createTheme } from '@mui/material';
+import type { AppProps } from 'next/app';
+import { useState } from 'react';
+import { MdDarkMode } from 'react-icons/md';
+import { CiLight } from 'react-icons/ci';
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [modePage, setModePage] = useState<PaletteMode>('light');
+  const theme = createTheme({
+    typography: {
+      fontFamily: 'poppins',
+    },
+    palette: {
+      mode: modePage,
+    },
+  });
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div>
+        <button
+          onClick={() => {
+            setModePage((prev) => (prev === 'dark' ? 'light' : 'dark'));
+          }}
+          className={`${
+            modePage === 'dark' ? 'text-black bg-white' : 'text-white bg-black'
+          }  rounded-br-3xl p-3`}
+        >
+          {modePage === 'dark' ? <CiLight size="20px" /> : <MdDarkMode />}
+        </button>
+        <Component {...pageProps} />
+      </div>
+    </ThemeProvider>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
-// import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className="bg-gray-200 w-full h-full">
       <Link href={'/main'}>
-        <button className="start bg-blue-400 px-3 py-2 text-white rounded-md hover:bg-blue-500 hover:shadow-md hover:transition-all font-bold">
+        <button className="start bg-blue-400 px-4 py-3 text-white rounded-md hover:bg-blue-500 hover:shadow-md hover:transition-all text-2xl font-bold">
           <div>Start</div>
         </button>
       </Link>
